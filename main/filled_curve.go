0862d0d6c46b5e9f99af3b471000dd03f7a2d5f4
Toru Hisai <toru@torus.jp>
2025-05-17 17:17:57 +0900

    make filled_curve game object

---
+++ main/filled_curve.go
@@ -0,0 +1,11 @@
+embedded_components {
+  id: "model"
+  type: "model"
+  data: "mesh: \"/builtins/assets/meshes/quad_2x2.dae\"\n"
+  "name: \"{{NAME}}\"\n"
+  "materials {\n"
+  "  name: \"default\"\n"
+  "  material: \"/main/filled_curve.material\"\n"
+  "}\n"
+  ""
+}
