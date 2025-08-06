4093ab77f51c1e3d51780395accb3f158480ea63
Toru Hisai <toru@torus.jp>
2025-08-06 11:45:46 +0900

    split into 2 collections

---
+++ main/main.go
@@ -0,0 +1,16 @@
+components {
+  id: "main"
+  component: "/main/main.script"
+}
+embedded_components {
+  id: "filled_curve_collection"
+  type: "collectionproxy"
+  data: "collection: \"/main/filled_curve.collection\"\n"
+  ""
+}
+embedded_components {
+  id: "bloch_sphare_collection"
+  type: "collectionproxy"
+  data: "collection: \"/main/bloch_sphare.collection\"\n"
+  ""
+}
